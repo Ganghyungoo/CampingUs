c9bd9da389684a0ed61d12a9f50ce45253c9bfab
nueijeel <lge6716@gmail.com>
2023-09-02 01:36:56 +0900

    fix : OrderProductModel 수정

--- CampingUsProject-customer/app/src/main/java/com/test/campingusproject_customer/dataclassmodel/OrderProductModel.kt
+++ CampingUsProject-customer/app/src/main/java/com/test/campingusproject_customer/dataclassmodel/OrderProductModel.kt
@@ -2,9 +2,13 @@ package com.test.campingusproject_customer.dataclassmodel
 
 data class OrderProductModel (
     val orderId : String,               //주문 ID
+    val orderProductId : Long,          //주문 상품 ID
+    val orderSellerId : String,         //주문 상품 판매자 ID
+    val orderDate : String,             //주문 날짜
+    val orderUserId : String,           //유저ID
     val orderProductName : String,      //상품 이름
     val orderProductCount : String,     //상품 개수
     val orderProductPrice : String,     //상품 가격
-    val orderProductImage : String,      //상품 이미지
-    val orderProductState : String      //배송상태..?
+    val orderProductImage : String,     //상품 이미지
+    val orderProductState : String      //배송상태
 )
